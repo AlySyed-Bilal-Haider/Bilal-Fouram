ae8bd1deef2d08b98440da8a916d7d47a60b4c65
aakash147 <akashrahim1122@gmail.com>
2022-09-21 18:50:58 +0500

    profile pages added

--- src/components/UserPenal/Discussion.jsx
+++ src/components/UserPenal/Discussion.jsx
@@ -1,9 +1,85 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
+import { FaRegComment } from "react-icons/fa";
 
 export default function Discussion() {
   return (
-    <>
-      <Box color="#000">Discussion box</Box>
-    </>
+    <Box pb={10}>
+      {["TestPool", "Testing"].map((item, i) => {
+        return (
+          <Box
+            key={i}
+            p={2}
+            sx={{
+              opacity: "0.4",
+              "&:hover": {
+                background: "#fff",
+                borderRadius: "10px",
+              },
+            }}
+          >
+            <Box
+              display="flex"
+              justifyContent="space-between"
+              alignItems="center"
+            >
+              <Typography
+                variant="body1"
+                color="primary.main"
+                fontWeight="700"
+                fontSize="18px"
+              >
+                {item}
+              </Typography>
+
+              <Box display="flex" alignItems="center">
+                <Typography
+                  variant="body1"
+                  component="span"
+                  color="text.main"
+                  backgroundColor="primary.main"
+                  borderRadius="5px"
+                  px="7px"
+                  py="1px"
+                >
+                  General
+                </Typography>
+                <FaRegComment
+                  style={{ marginLeft: "15px", marginRight: "5px" }}
+                />{" "}
+                0
+              </Box>
+            </Box>
+
+            <Box>
+              <Box display="flex" alignItems="center">
+                <Typography
+                  variant="subtitle2"
+                  color="primary.main"
+                  fontWeight="700"
+                >
+                  MajorSaab
+                </Typography>
+                <Typography
+                  ml={1}
+                  variant="body1"
+                  color="primary.light"
+                  fontSize="13px"
+                >
+                  started 2 days ago
+                </Typography>
+              </Box>
+            </Box>
+
+            <Box mt={1} fontSize="14px" color="text.paragraph">
+              Before you post this:
+              <br />
+              i. The forum is intended for in-depth discussion only. For support
+              tickets or general queries, please head to our Discord channel:
+              https://discord.com/invite/olympusdao
+            </Box>
+          </Box>
+        );
+      })}
+    </Box>
   );
 }
--- src/components/UserPenal/Likes.jsx
+++ src/components/UserPenal/Likes.jsx
@@ -3,7 +3,9 @@ import { Box } from "@mui/material";
 export default function Like() {
   return (
     <>
-      <Box color="#000">Like box</Box>
+      <Box py={5} color="primary.light" fontSize="18px" textAlign="center">
+        It looks like there are no posts here.
+      </Box>
     </>
   );
 }
