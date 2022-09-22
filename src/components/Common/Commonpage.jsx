eab71eab063cd5d9dfae1db28ec0a4e61743212a
AlySyed-Bilal-Haider <bilalshahbscs@gmail.com>
2022-09-22 11:05:09 +0500

    solve error in common component

--- src/components/Common/Commonpage.jsx
+++ src/components/Common/Commonpage.jsx
@@ -12,7 +12,7 @@ function Commonpage(props) {
   const [namestate,setnamestate]=useState(localStorage.getItem("name"));
   const [alldetailsstate, setDetailsState] = React.useState();
   const [categorystate,setCategorystate]=useState([]);
-  const [allDescussions,setAlldescussionsstate]=useState([]);
+  const [allDescussionsstate,setAlldescussionsstate]=useState([]);
 
   // fetch specific details from server
   useEffect(() => {
@@ -22,11 +22,11 @@ function Commonpage(props) {
         console.log("data",data);
        setCategorystate(data)
       } catch (error) {
-        console.log(error);
+        console.log("commonpage category error:",error);
       }
     };
     fetchcategory();
-  }, [props.tage]);
+  }, [props.title]);
 
   // fetch all details from sever
   useEffect(() => {
@@ -34,6 +34,7 @@ function Commonpage(props) {
       try {
         const { data } = await axios.get(`${url}/alldiscussion`);
         setAlldescussionsstate(data.allDiscussion);
+        console.log("data details",data.allDiscussion);
       } catch (error) {
         console.log(error);
       }
@@ -42,10 +43,10 @@ function Commonpage(props) {
   }, []);
 
   useEffect(()=>{
- if(categorystate){
+ if(categorystate.length !==0){
   setDetailsState(categorystate);
  }else{
-  setDetailsState(allDescussions);
+  setDetailsState(allDescussionsstate);
  }
   },[categorystate])
   return (
@@ -86,132 +87,137 @@ function Commonpage(props) {
       </Box>
       {/* 
       .........start main sections here ............. */}
-      {alldetailsstate?.map((items, i) => {
-        return (
-          <Box
-            key={i}
-            sx={{
-              mt: 2,
-              pt: 0.5,
-              pb: { md: 1, xs: 0 },
-              pr: 4,
-              "&:hover": {
-                backgroundColor: "text.main",
-                borderRadius: "4px",
-                cursor: "pointer",
-              },
-            }}
-          >
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-flex",
-                alignItems: { md: "center", xs: "flex-start" },
-                p: { md: 2, xs: 1 },
-              }}
-            >
-              <Avatar
-                sx={{
-                  width: { md: "40px", xs: "30px" },
-                  height: { md: "40px", xs: "30px" },
-                  bgcolor: deepPurple[500],
-                  mr: { md: 2, xs: 1 },
-                  fontSize: { md: "14px", xs: "12px" },
-                }}
-              >
-                OP
-              </Avatar>
+      {
+        props.title && categorystate.length ==0 ? (<Typography variant="h4" mt={2} fontWeight={700} align="center">Upcoming as soon {  props.title } !</Typography>):(
+          alldetailsstate?.map((items, i) => {
+            return (
               <Box
+                key={i}
                 sx={{
-                  width: "100%",
-                  display: "flex",
-                  justifyContent: "space-between",
-                  alignItems: "flex-start",
-                  flexDirection: { md: "row", xs: "column" },
+                  mt: 2,
+                  pt: 0.5,
+                  pb: { md: 1, xs: 0 },
+                  pr: 4,
+                  "&:hover": {
+                    backgroundColor: "text.main",
+                    borderRadius: "4px",
+                    cursor: "pointer",
+                  },
                 }}
               >
-                <Box>
-                  <Typography
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-flex",
+                    alignItems: { md: "center", xs: "flex-start" },
+                    p: { md: 2, xs: 1 },
+                  }}
+                >
+                  <Avatar
                     sx={{
-                      color: "text.paragraph",
-                      fontSize: { md: "16px", xs: "12px" },
+                      width: { md: "40px", xs: "30px" },
+                      height: { md: "40px", xs: "30px" },
+                      bgcolor: deepPurple[500],
+                      mr: { md: 2, xs: 1 },
+                      fontSize: { md: "14px", xs: "12px" },
                     }}
                   >
-                    {items.title}
-                  </Typography>
+                    OP
+                  </Avatar>
                   <Box
                     sx={{
+                      width: "100%",
                       display: "flex",
-                      alignItems: "center",
-                      flexWrap: "wrap",
+                      justifyContent: "space-between",
+                      alignItems: "flex-start",
+                      flexDirection: { md: "row", xs: "column" },
                     }}
                   >
-                    <UndoIcon sx={{ color: "text.paragraph" }} />
-                    <Typography
-                      sx={{ color: "text.paragraph", fontSize: "11px" }}
-                    >
-                      <strong>
-                        {namestate
-                          ? namestate 
-                          : "Usman Shab"}
-                      </strong>{" "}
-                      {items.enddate ? items?.enddate.toString() : new Date()}
-                    </Typography>
+                    <Box>
+                      <Typography
+                        sx={{
+                          color: "text.paragraph",
+                          fontSize: { md: "16px", xs: "12px" },
+                        }}
+                      >
+                        {items.title}
+                      </Typography>
+                      <Box
+                        sx={{
+                          display: "flex",
+                          alignItems: "center",
+                          flexWrap: "wrap",
+                        }}
+                      >
+                        <UndoIcon sx={{ color: "text.paragraph" }} />
+                        <Typography
+                          sx={{ color: "text.paragraph", fontSize: "11px" }}
+                        >
+                          <strong>
+                            {namestate
+                              ? namestate 
+                              : "Usman Shab"}
+                          </strong>{" "}
+                          {items.enddate ? items?.enddate.toString() : new Date()}
+                        </Typography>
+                      </Box>
+                    </Box>
+                    <Box>
+                      <Button
+                        startIcon={
+                          <AutorenewIcon
+                            sx={{ fontSize: { md: "8px", xs: "4px" } }}
+                          />
+                        }
+                        sx={{
+                          height: "17px",
+                          fontSize: "8px",
+                          fontWeight: 700,
+                          backgroundColor: "primary.main",
+                          borderRadius: "4px",
+                          color: "text.main",
+                          px: { md: 1, xs: 0.5 },
+                          py: { md: 1.5, xs: 1 },
+                          "&:hover": {
+                            backgroundColor: "primary.main",
+                          },
+                        }}
+                      >
+                        {props.title ? props.title : "All descussion"}
+                      </Button>
+                      <Button
+                        startIcon={
+                          <AutorenewIcon
+                            sx={{
+                              fontSize: "14px",
+                              display: { md: "block", xs: "none" },
+                            }}
+                          />
+                        }
+                        sx={{ height: "21px", fontSize: "14px" }}
+                      >
+                        0
+                      </Button>
+                    </Box>
                   </Box>
                 </Box>
-                <Box>
-                  <Button
-                    startIcon={
-                      <AutorenewIcon
-                        sx={{ fontSize: { md: "8px", xs: "4px" } }}
-                      />
-                    }
-                    sx={{
-                      height: "17px",
-                      fontSize: "8px",
-                      fontWeight: 700,
-                      backgroundColor: "primary.main",
-                      borderRadius: "4px",
-                      color: "text.main",
-                      px: { md: 1, xs: 0.5 },
-                      py: { md: 1.5, xs: 1 },
-                      "&:hover": {
-                        backgroundColor: "primary.main",
-                      },
-                    }}
-                  >
-                    {props.title ? props.title : "All descussion"}
-                  </Button>
-                  <Button
-                    startIcon={
-                      <AutorenewIcon
-                        sx={{
-                          fontSize: "14px",
-                          display: { md: "block", xs: "none" },
-                        }}
-                      />
-                    }
-                    sx={{ height: "21px", fontSize: "14px" }}
-                  >
-                    0
-                  </Button>
-                </Box>
+                <Typography
+                  sx={{
+                    color: "text.paragraph",
+                    fontSize: "11px",
+                    width: "85%",
+                    m: "auto",
+                    display: { md: "block", xs: "none" },
+                  }}
+                >
+                  {items.description}
+                </Typography>
               </Box>
-            </Box>
-            <Typography
-              sx={{
-                color: "text.paragraph",
-                fontSize: "11px",
-                width: "85%",
-                m: "auto",
-                display: { md: "block", xs: "none" },
-              }}
-            >
-              {items.description}
-            </Typography>
-          </Box>
-        );
-      })}
+            );
+          })
+        )
+      
+   }
     </Grid>
   );
 }
